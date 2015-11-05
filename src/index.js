950db79d06322d79182aa3e327def18ade53e4c0
Goran Gajic <fero.null@gmail.com>
2015-11-05 16:28:32 +0100

    add single include of all modules

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export MorphReplace from './MorphReplace';
+export MorphReplaceResize from './MorphReplaceResize';
+export MorphTransition from './MorphTransition';
+export MorphTransitionResize from './MorphTransitionResize';
+export MorphReplaceNative from './MorphReplaceNative';
+export MorphTransitionNative from './MorphTransitionNative';
